65707230da3c44d5160cd599363e94f99d61975c
Patricio <89046592+patriciogsilva@users.noreply.github.com>
2021-11-09 10:11:19 -0300

    Add files via upload

---
+++ Prog-Imperativa/Aula08/mesa DT.js
@@ -0,0 +1,22 @@
+function podeSubir(altura, acompanhada) {
+    let podeSubir = false
+
+    if (altura >= 1.4 && altura <= 2.0) {
+        podeSubir = true
+        console.log("Acesso Autorizado, pode subir.")
+    }
+
+    else if (altura >= 1.2 && acompanhada) {
+        podeSubir = true
+        console.log("Acesso autorizado com o acompanhante.")
+    }
+
+    else {
+        console.log("não poderá subir, nem acompanhada.")
+    }
+
+    return podeSubir
+
+}
+
+console.log(podeSubir(1.4, true));
